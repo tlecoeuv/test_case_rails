e32c6ba3bc2db51b898f93abe04e81ae8c3153fb
tlecoeuv <tanguy.lecoeuvre@gmail.com>
2022-06-02 09:13:14 +0200

    validation added to model

--- user_api/app/models/user.rb
+++ user_api/app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+
+	class EmailValidator < ActiveModel::EachValidator
+		def validate_each(record, attribute, value)
+			unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+				record.errors.add attribute, (options[:message] || "is not a valid email")
+			end
+		end
+	end
+
+	class PhoneValidator < ActiveModel::EachValidator
+		def validate_each(record, attribute, value)
+			unless value =~ /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/
+				record.errors.add attribute, (options[:message] || "is not a valid french phone number")
+			end
+		end
+	end
+
+	validates :email, presence: true, uniqueness: true, email: true
+	validates :first_name, presence: true, length: {minimum: 3}
+	validates :last_name, presence: true, length: {minimum: 3}
+	validates :phone, phone: true
 end
